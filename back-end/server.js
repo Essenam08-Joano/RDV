daac3761d59e254e954ea584f9fc3ad8a206ffce
Essanam Joanice <persidejoanicealotcho@gmail.com>
2026-08-14 00:37:47 +0100

    Mise à jour du fichier server.js q
    q

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../front-end')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Fichier de stockage
 const DATA_FILE = path.join(__dirname, 'data.json');
