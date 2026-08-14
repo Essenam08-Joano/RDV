d6b9f73e4e8b10c653626d96e84bd2e669bd5dac
Essenam Joanice <persidejoanicealotcho@gmail.com>
2026-08-14 20:40:17 +0100

    Fix static file path for frontend directory

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../front-end')));
 
 // Fichier de stockage
 const DATA_FILE = path.join(__dirname, 'data.json');
